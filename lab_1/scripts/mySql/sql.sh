d77edb299a567006131782c679ba4bff6f70cb33
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-11-24 21:27:54 +0100

    Possibly working database

--- lab_1/scripts/mySql/sql.sh
+++ lab_1/scripts/mySql/sql.sh
@@ -9,11 +9,17 @@ DATABASE_PASSWORD=admin
 # DATABASE_USER=$2
 # DATABASE_PASSWORD=$3
 
+DATABASE_CONFIG="./wus-22z/lab_1/scripts/mySql/.my.cnf"
+
 cd ~/
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt install mysql-server=5.7.8 -y
+sudo apt install mariadb-server-10.3 -y
 
+git clone https://github.com/bartlomiejkrawczyk/WUS-22Z.git
 
+cp $DATABASE_CONFIG ~/.my.cnf
+
+sudo service mysql restart
---
+++ lab_1/scripts/nginx/nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install nginx
+
+sudo systemctl start nginx
+
+
+
+
