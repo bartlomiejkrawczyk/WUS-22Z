eb2f5f2dee158e034eeea9dda762a53f1e36e698
kkasper1 <karol.kasperek.stud@pw.edu.pl>
2022-12-02 22:21:41 +0100

    wywołanie backendu z argumentami

--- lab_1/scripts/rest/spring.sh
+++ lab_1/scripts/rest/spring.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-SERVER_PORT=8080
-DATABASE_ADDRESS=localhost
-DATABASE_PORT=3306
-DATABASE_USER=admin
-DATABASE_PASSWORD=admin
+if [ "$#" -ne 5 ]; then
+    echo "Potrzebne dane do użycia skryptu: $0 SERVER_PORT DATABASE_ADDRESS DATABASE_PORT DATABASE_USER DATABASE_PASSWORD" >&2
+    exit 1
+fi
 
-# SERVER_PORT=$1
-# DATABASE_ADDRESS=$2
-# DATABASE_PORT=$3
-# DATABASE_USER=$4
-# DATABASE_PASSWORD=$5
+SERVER_PORT="$1"
+DATABASE_ADDRESS="$2"
+DATABASE_PORT="$3"
+DATABASE_USER="$4"
+DATABASE_PASSWORD="$5"
 
 SPRING_CONFIG="./wus-22z/lab_1/scripts/rest/application.properties"
 SERVER_CONFIG="./spring-petclinic-rest/src/main/resources/application.properties"
@@ -21,8 +20,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install openjdk-8-jdk
 java -version
-# sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_version/bin/java
-# java -version
 
 git clone https://github.com/bartlomiejkrawczyk/WUS-22Z.git
 
@@ -40,6 +37,3 @@ sed -i "s/DATABASE_PASSWORD/$DATABASE_PASSWORD/g" $SERVER_CONFIG
 cd ./spring-petclinic-rest/
 
 ./mvnw spring-boot:run
-
-
-
