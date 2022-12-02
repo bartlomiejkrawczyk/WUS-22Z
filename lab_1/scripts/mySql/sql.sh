827c67e70bf580a6509f4d4ebc6316bf02889559
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-02 22:51:32 +0100

    Add database script

--- lab_1/scripts/mySql/sql.sh
+++ lab_1/scripts/mySql/sql.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
-DATABASE_PORT=3306
+set -euxo pipefail
 
-DATABASE_USER=admin
-DATABASE_PASSWORD=admin
+if [ $# -lt 3 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo "Usage: $0 DATABASE_PORT DATABASE_USER DATABASE_PASSWORD"
+  exit 1
+fi
 
-# DATABASE_PORT=$1
-# DATABASE_USER=$2
-# DATABASE_PASSWORD=$3
+DATABASE_PORT=$1
+DATABASE_USER=$2
+DATABASE_PASSWORD=$3
 
-DATABASE_CONFIG="./wus-22z/lab_1/scripts/mySql/.my.cnf"
+MY_SQL_CONFIG="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/mySql/.my.cnf"
+USER_DATABASE="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/mySql/user.sql"
+INIT_DATABASE="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
+POPULATE_DATABASE="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
 
 cd ~/
 
+# Instalation
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt install mariadb-server-10.3 -y
+sudo apt install mysql-server -y
+sudo apt install wget -y
 
-git clone https://github.com/bartlomiejkrawczyk/WUS-22Z.git
+# Download config files
+wget $MY_SQL_CONFIG
+wget $USER_DATABASE
+wget $INIT_DATABASE
+wget $POPULATE_DATABASE
 
-cp $DATABASE_CONFIG ~/.my.cnf
+# Update configuration
+sed -i "s/DATABASE_PORT/$DATABASE_PORT/g" ./.my.cnf
+sed -i "s/DATABASE_USER/$DATABASE_USER/g" ./user.sql
+sed -i "s/DATABASE_PASSWORD/$DATABASE_PASSWORD/g" ./user.sql
 
+# Run sql
+cat ./user.sql | sudo mysql -f
+cat ./initDB.sql | sudo mysql -f
+cat ./populateDB.sql | sudo mysql -f
+
+# Restart service
 sudo service mysql restart
