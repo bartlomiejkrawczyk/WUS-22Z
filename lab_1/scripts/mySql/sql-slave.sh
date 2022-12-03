66a769fe11a593fcb450469fccfc5790e5ae719d
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-03 22:19:08 +0100

    Update mysql slave

--- lab_1/scripts/mySql/sql-slave.sh
+++ lab_1/scripts/mySql/sql-slave.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 5 ]; then
   echo 1>&2 "$0: not enough arguments"
   echo "Usage: $0 DATABASE_PORT DATABASE_USER DATABASE_PASSWORD MASTER_DATABASE_ADDRESS MASTER_DATABASE_PORT"
   exit 1
@@ -14,7 +14,7 @@ DATABASE_PASSWORD=$3
 MASTER_DATABASE_ADDRESS=$4
 MASTER_DATABASE_PORT=$5
 
-MY_SQL_CONFIG="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/mySql/.my2.cnf"
+MY_SQL_CONFIG="/etc/mysql/mysql.conf.d/mysqld.cnf"
 USER_DATABASE="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/mySql/user.sql"
 INIT_DATABASE="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
 POPULATE_DATABASE="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
@@ -24,18 +24,21 @@ cd ~/
 # Instalation
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt-get autoremove -y
 
-sudo apt install mysql-server -y
-sudo apt install wget -y
+sudo apt-get install mysql-server -y
+sudo apt-get install wget -y
 
 # Download config files
-wget $MY_SQL_CONFIG -o ./.my.cnf
 wget $USER_DATABASE
 wget $INIT_DATABASE
 wget $POPULATE_DATABASE
 
 # Update configuration
-sed -i "s/DATABASE_PORT/$DATABASE_PORT/g" ./.my.cnf
+echo port = $DATABASE_PORT >> $MY_SQL_CONFIG
+echo server-id = 2 >> $MY_SQL_CONFIG
+
+sed -i "s/127.0.0.1/0.0.0.0/g" $MY_SQL_CONFIG
 sed -i "s/DATABASE_USER/$DATABASE_USER/g" ./user.sql
 sed -i "s/DATABASE_PASSWORD/$DATABASE_PASSWORD/g" ./user.sql
 sed -i "1s/^/USE petclinic;\n/" ./populateDB.sql
@@ -50,4 +53,7 @@ sudo service mysql restart
 
 # Change master
 
-sudo mysql -e "CHANGE MASTER TO MASTER_HOST='$MASTER_DATABASE_ADDRESS', MASTER_PORT=$MASTER_DATABASE_PORT, MASTER_USER='$DATABASE_USER', MASTER_PASSWORD='$DATABASE_PASSWORD';start slave;"
+sudo mysql -e "CHANGE MASTER TO MASTER_HOST='$MASTER_DATABASE_ADDRESS', MASTER_PORT=$MASTER_DATABASE_PORT, MASTER_USER='$DATABASE_USER', MASTER_PASSWORD='$DATABASE_PASSWORD';"
+sudo mysql -e "start slave;"
+
+echo DONE
