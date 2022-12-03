54b8f9a4a191c03dafa550dc081a927c50a8d9d7
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-03 14:21:00 +0100

    Probably working nginx script

--- lab_1/scripts/nginx/nginx.sh
+++ lab_1/scripts/nginx/nginx.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 NGINX_PORT READ_SERVER_ADDRESS READ_SERVER_PORT WRITE_SERVER_ADDRESS WRITE_SERVER_PORT" >&2
+    exit 1
+fi
+
+NGINX_PORT=$1
+READ_SERVER_ADDRESS=$2
+READ_SERVER_PORT=$3
+WRITE_SERVER_ADDRESS=$3
+WRITE_SERVER_PORT=$3
+
+NGINX_CONFIG="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/nginx/nginx.conf"
+
+# Instalation
 sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt install nginx -y
+sudo apt install wget -y
 
-NGINX_CONFIG="./wus-22z/lab_1/scripts/nginx/nginx.conf"
+# Download configuration
+wget $NGINX_CONFIG
 
-git clone https://github.com/bartlomiejkrawczyk/WUS-22Z.git
+# Update configuration
+sed -i "s/NGINX_PORT/$NGINX_PORT/g" ./nginx.conf
+sed -i "s/READ_SERVER_ADDRESS/$READ_SERVER_ADDRESS/g" ./nginx.conf
+sed -i "s/READ_SERVER_PORT/$READ_SERVER_PORT/g" ./nginx.conf
+sed -i "s/WRITE_SERVER_ADDRESS/$WRITE_SERVER_ADDRESS/g" ./nginx.conf
+sed -i "s/WRITE_SERVER_PORT/$WRITE_SERVER_PORT/g" ./nginx.conf
 
-sudo cp $NGINX_CONFIG /etc/nginx/nginx.conf
+sudo cp ./nginx.conf /etc/nginx/nginx.conf
 
+# Restart service
 sudo systemctl start nginx
-
-
-
-
