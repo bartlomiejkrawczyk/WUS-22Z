cb96031246c2f511589169bee00861dc1dc6c10b
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-03 20:31:12 +0100

    Working front

--- lab_1/scripts/angular/front.sh
+++ lab_1/scripts/angular/front.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 SERVER_IP SERVER_PORT FRONT_PORT" >&2
     exit 1
@@ -13,28 +15,33 @@ FRONT_PORT="$3"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt autoremove -y
-sudo apt-get install -y npm
-sudo apt-get install nodejs -y
+sudo apt-get install curl -y
+sudo apt-get install npm -y
+
+export HOME=/root
+
+# Install package manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 12.11.1
 
 # Clone project
 cd ~/
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 cd spring-petclinic-angular/
 
-# Install node js
-echo N | sudo npm install -g @angular/cli@11.2.11
-echo N | ng analytics off
-echo N | sudo npm install angular-http-server
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-nvm install 12.11.1
-
 # Update configuration
 sed -i "s/localhost/$SERVER_IP/g" src/environments/environment.ts src/environments/environment.prod.ts
 sed -i "s/9966/$SERVER_PORT/g" src/environments/environment.ts src/environments/environment.prod.ts
+
+echo N | npm install -g @angular/cli@latest
+echo N | npm install
+echo N | ng analytics off
+
+npm install angular-http-server
 
 # Build project
 npm run build -- --prod
