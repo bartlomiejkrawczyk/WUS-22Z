184f5ed7934025a072bf8aa16b0f5d3158918569
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-03 15:20:42 +0100

    Update frontend script

--- lab_1/scripts/angular/front.sh
+++ lab_1/scripts/angular/front.sh
@@ -9,22 +9,35 @@ SERVER_IP="$1"
 SERVER_PORT="$2"
 FRONT_PORT="$3"
 
+# Instalation
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt autoremove -y
 sudo apt-get install -y npm
 sudo apt-get install nodejs -y
 
-echo N | npm install -g @angular/cli@11.2.11
-echo N | ng analytics off
-
+# Clone project
+cd ~/
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
-
 cd spring-petclinic-angular/
+
+# Install node js
+echo N | sudo npm install -g @angular/cli@11.2.11
+echo N | ng analytics off
+echo N | sudo npm install angular-http-server
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install 12.11.1
+
+# Update configuration
 sed -i "s/localhost/$SERVER_IP/g" src/environments/environment.ts src/environments/environment.prod.ts
 sed -i "s/9966/$SERVER_PORT/g" src/environments/environment.ts src/environments/environment.prod.ts
 
-npm install angular-http-server
-
+# Build project
 npm run build -- --prod
 
-npx angular-http-server --path ./dist -p $FRONT_PORT
+# Start server
+npx angular-http-server --path ./dist -p $FRONT_PORT &
