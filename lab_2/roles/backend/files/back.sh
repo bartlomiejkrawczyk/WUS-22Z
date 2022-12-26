b77861a2152b76b698773faa37ecf91118c93850
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-27 00:00:38 +0100

    Divide tasks by roles

---
+++ lab_2/roles/backend/files/back.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -euxo pipefail
+# TODO: remove me ;P
+echo BACK
+exit 0
+
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 SERVER_PORT DATABASE_ADDRESS DATABASE_PORT DATABASE_USER DATABASE_PASSWORD" >&2
+    exit 1
+fi
+
+DIRECTORY="$RANDOM"
+
+echo $DIRECTORY
+
+SERVER_PORT="$1"
+DATABASE_ADDRESS="$2"
+DATABASE_PORT="$3"
+DATABASE_USER="$4"
+DATABASE_PASSWORD="$5"
+
+SPRING_CONFIG="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/rest/application.properties"
+SERVER_CONFIG="./spring-petclinic-rest/src/main/resources/application.properties"
+
+cd ~/
+
+mkdir $DIRECTORY
+
+cd $DIRECTORY/
+
+# Download project
+git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
+
+wget $SPRING_CONFIG -O $SERVER_CONFIG
+
+# Update configuration
+sed -i "s/SERVER_PORT/$SERVER_PORT/g" $SERVER_CONFIG
+sed -i "s/DATABASE_ADDRESS/$DATABASE_ADDRESS/g" $SERVER_CONFIG
+sed -i "s/DATABASE_PORT/$DATABASE_PORT/g" $SERVER_CONFIG
+sed -i "s/DATABASE_USER/$DATABASE_USER/g" $SERVER_CONFIG
+sed -i "s/DATABASE_PASSWORD/$DATABASE_PASSWORD/g" $SERVER_CONFIG
+
+# Test and run project
+cd ./spring-petclinic-rest/
+# ./mvnw test # TODO: uncomment me :)
+./mvnw spring-boot:run &
+
+echo DONE
---
+++ lab_2/roles/backend_replicaset/files/back-replicaset.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -euxo pipefail
+# TODO: remove me ;P
+echo BACK
+exit 0
+
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 SERVER_PORT DATABASE_MASTER_ADDRESS DATABASE_MASTER_PORT DATABASE_SLAVE_ADDRESS DATABASE_SLAVE_PORT DATABASE_USER DATABASE_PASSWORD" >&2
+    exit 1
+fi
+
+DIRECTORY="$RANDOM"
+
+echo $DIRECTORY
+
+SERVER_PORT="$1"
+DATABASE_MASTER_ADDRESS="$2"
+DATABASE_MASTER_PORT="$3"
+DATABASE_SLAVE_ADDRESS="$4"
+DATABASE_SLAVE_PORT="$5"
+DATABASE_USER="$6"
+DATABASE_PASSWORD="$7"
+
+SPRING_CONFIG="https://raw.githubusercontent.com/bartlomiejkrawczyk/WUS-22Z/master/lab_1/scripts/rest/application-replicaset.properties"
+SERVER_CONFIG="./spring-petclinic-rest/src/main/resources/application.properties"
+
+cd ~/
+
+mkdir $DIRECTORY
+
+cd $DIRECTORY/
+
+# Download project
+git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
+
+wget $SPRING_CONFIG -O $SERVER_CONFIG
+
+# Update configuration
+sed -i "s/SERVER_PORT/$SERVER_PORT/g" $SERVER_CONFIG
+sed -i "s/DATABASE_MASTER_ADDRESS/$DATABASE_MASTER_ADDRESS/g" $SERVER_CONFIG
+sed -i "s/DATABASE_MASTER_PORT/$DATABASE_MASTER_PORT/g" $SERVER_CONFIG
+sed -i "s/DATABASE_SLAVE_ADDRESS/$DATABASE_SLAVE_ADDRESS/g" $SERVER_CONFIG
+sed -i "s/DATABASE_SLAVE_PORT/$DATABASE_SLAVE_PORT/g" $SERVER_CONFIG
+sed -i "s/DATABASE_USER/$DATABASE_USER/g" $SERVER_CONFIG
+sed -i "s/DATABASE_PASSWORD/$DATABASE_PASSWORD/g" $SERVER_CONFIG
+
+# Test and run project
+cd ./spring-petclinic-rest/
+./mvnw test
+./mvnw spring-boot:run &
+
+echo DONE
