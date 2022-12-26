db0fcbe3ce26f814a33b249d4e6001226e64f08d
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-26 22:29:54 +0100

    Copy code used to delete vms

---
+++ lab_2/vms/delete.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo "Usage: $0 CONFIG_FILE"
+  exit 2
+fi
+
+CONFIG_FILE="$1"
+
+RESOURCE_GROUP="$(cat $CONFIG_FILE | jq -r '.resource_group')"
+
+# Instalation
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install jq -y
+sudo apt-get install azure-cli -y
+
+# Login
+az login
+
+# Delete
+az group delete --name $RESOURCE_GROUP -y
+
+az group delete --name NetworkWatcherRG -y
+
+# Logout
+az logout
