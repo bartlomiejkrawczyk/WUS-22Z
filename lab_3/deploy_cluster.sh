20aeb7b1170a7a9e7ff357753ede38c5531b0bc6
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-01-25 00:40:56 +0100

    add monitoring

--- lab_3/deploy_cluster.sh
+++ lab_3/deploy_cluster.sh
@@ -10,16 +10,21 @@ az login
 # Resource Group
 az group create --name $RESOURCE_GROUP --location westeurope
 
+# Cluster
 az aks create --resource-group $RESOURCE_GROUP \
     --name wusCluster \
     --enable-managed-identity \
     --node-count 2 \
     --generate-ssh-keys
 
-# sudo az aks install-cli
+az aks enable-addons --addons monitoring \
+    --resource-group $RESOURCE_GROUP \
+    --name wusCluster
 
+# Update kubectl configuration
 az aks get-credentials \
     --resource-group $RESOURCE_GROUP \
     --name wusCluster
 
+# Check whether nodes are up
 kubectl get nodes
--- lab_3/deploy_petclinic.sh
+++ lab_3/deploy_petclinic.sh
@@ -11,7 +11,7 @@ kubectl apply -f k8s/init-services
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm search repo bitnami
+# helm search repo bitnami
 
 helm install vets-db-mysql bitnami/mysql --namespace spring-petclinic --version 9.4.6 --set auth.database=service_instance_db
 helm install visits-db-mysql bitnami/mysql --namespace spring-petclinic  --version 9.4.6 --set auth.database=service_instance_db
@@ -20,5 +20,9 @@ helm install customers-db-mysql bitnami/mysql --namespace spring-petclinic  --ve
 export REPOSITORY_PREFIX=springcommunity
 
 ./scripts/deployToKubernetes.sh
+
+kubectl get svc -n spring-petclinic
+
+kubectl get pods --all-namespaces
 
 kubectl get svc -n spring-petclinic api-gateway
